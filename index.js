322fee5c3f4e863eece4d73855933bc53ef0cbba
valdi7913 <valdi7913@gmail.com>
2024-04-23 20:27:45 +0000

    First version finished

--- index.js
+++ index.js
@@ -1,36 +1,72 @@
 const darkLink = 'dark';
 const lightLink = 'light';
-let isLight = true;
+
+let isLight = localStorage.getItem('isLight');
+
+// If not set default to light mode
+if (isLight === null) {isLight = true;} 
+else {isLight = isLight === 'true'; }
+
+applyTheme(isLight);
+
 const sunMoonButton = document
     .getElementById("mode")
-    .addEventListener( 'click', (e) => {
-        console.log(e.target.classList);
-        e.target.classList.toggle("flip");
-        if(isLight) {
-            addCss(darkLink);
-            removeCss(lightLink);
-        } else {
-            addCss(lightLink);
-            removeCss(darkLink);
-        }
+    .addEventListener('click', (e) => {
         isLight = !isLight;
+        applyTheme(isLight);
+        localStorage.setItem('isLight', isLight);
     });
+    
+document.addEventListener("DOMContentLoaded", function() {
+  // Get all links that point to sections on the same page
+  const links = document.querySelectorAll('a[href^="#"]');
+  
+  // Add click event listener to each link
+  links.forEach(link => {
+    link.addEventListener('click', function(e) {
+      const targetId = this.getAttribute('href'); // Get the target section's id
+      const targetSection = document.querySelector(targetId); // Get the target section
+      
+      // Add jumping effect class to the target section
+      targetSection.classList.add('jumping-effect');
+      
+      // Remove the jumping effect class after 500ms
+      setTimeout(() => {
+        targetSection.classList.remove('jumping-effect');
+      }, 2000); // Adjust the time according to the animation duration
+    });
+  });
+});
+    
+
+if(isLight && !sunMoonButton.classList.contains("flip")) sunMoonButton.classList.toggle("flip");
+
+function applyTheme(isLight) {
+    if (isLight) {
+        addCss(lightLink);
+        removeCss(darkLink);
+    } else {
+        addCss(darkLink);
+        removeCss(lightLink);
+    }
+}
 
 function addCss(fileName) {
     const head = document.head;
     const link = document.createElement("link");
 
-    link.id=fileName;
-    link.type="text/css";
-    link.rel="stylesheet";
-    link.href=fileName+".css";
-    
+    link.id = fileName;
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    link.href = fileName + ".css";
+
     head.appendChild(link);
 }
 
 function removeCss(fileName) {
-    console.log('removing element with id ', fileName)
     const sheet = document.getElementById(fileName);
-    sheet.disabled = true;
-    sheet.parentNode.removeChild(sheet);
+    if (sheet) {
+        sheet.disabled = true;
+        sheet.parentNode.removeChild(sheet);
+    }
 }
